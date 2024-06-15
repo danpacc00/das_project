dc35d6b926a8f6ad5d67b844e0488985a1f3a989
danpacc <paccusse.d@gmail.com>
2024-06-15 11:45:52 +0200

    Modifications on cost function corridor

--- task_2/ros/src/surveillance/surveillance/costs_fn.py
+++ task_2/ros/src/surveillance/surveillance/costs_fn.py
@@ -73,12 +73,60 @@ class CorridorCostV2:
 
         # find the obstacles which have the minimum distance to the agent
 
-        for i in range(self.obstacles.shape[0]):
-            li_obstacles_tmp = -np.log(np.linalg.norm(zz - self.obstacles[i]) ** 2 - self.dd)
+        obstacle_seen = list(
+            map(lambda x: x[1], sorted([(np.linalg.norm(zz - o) ** 2, o) for o in self.obstacles], key=lambda x: x[0]))
+        )
+
+        for obstacle in obstacle_seen:
+            li_obstacles_tmp = -0.5 * np.log(np.linalg.norm(zz - obstacle) ** 2 - self.dd)
 
             li_obstacles += li_obstacles_tmp
 
-            nabla_1 += -2 * (zz - self.obstacles[i]) / (np.linalg.norm(zz - self.obstacles[i]) ** 2 - self.dd)
+            nabla_1 += -2 * 0.5 * (zz - obstacle) / (np.linalg.norm(zz - obstacle) ** 2 - self.dd)
+
+        li = li_target + li_sigma + self.beta * li_obstacles + li_corridor
+        nabla_1 += 2 * self.alpha * (zz - target) + 2 * self.gamma * (zz - sigma)
+        nabla_2 = 2 * self.gamma * (zz - sigma) + 2 * np.linalg.norm(sigma[1] - self.mean_y)
+
+        return li, nabla_1, nabla_2
+
+
+class CorridorCostV3:
+    def __init__(self, alpha, beta, gamma, dd, obstacles):
+        self.alpha = alpha
+        self.beta = beta
+        self.gamma = gamma
+        self.dd = dd
+        self.obstacles = obstacles
+
+        # find maximum value of obstacles along y
+        max_y = np.max(self.obstacles[:, 1])
+        min_y = np.min(self.obstacles[:, 1])
+
+        # self.mean_y = abs((max_y + min_y)) / 2
+        self.mean_y = 0
+
+    def __call__(self, target, zz, sigma):
+        li_target = self.alpha * np.linalg.norm(zz - target) ** 2
+        li_sigma = self.gamma * np.linalg.norm(zz - sigma) ** 2
+
+        li_corridor = np.linalg.norm(sigma[1] - self.mean_y) ** 2
+
+        li_obstacles = 0
+        nabla_1 = 0
+
+        # find the obstacles which have the minimum distance to the agent
+
+        obstacle_seen = list(
+            map(lambda x: x[1], sorted([(np.linalg.norm(zz - o) ** 2, o) for o in self.obstacles], key=lambda x: x[0]))
+        )
+
+        for obstacle in obstacle_seen:
+            li_obstacles_tmp = -0.5 * np.log(np.linalg.norm(zz - obstacle) ** 2 - self.dd)
+
+            li_obstacles += li_obstacles_tmp
+
+            nabla_1 += -2 * 0.5 * (zz - obstacle) / (np.linalg.norm(zz - obstacle) ** 2 - self.dd)
 
         li = li_target + li_sigma + self.beta * li_obstacles + li_corridor
         nabla_1 += 2 * self.alpha * (zz - target) + 2 * self.gamma * (zz - sigma)
